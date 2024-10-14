846e99c64043825c04570ceaf977f14ccf65260e
Capataina <47505622+Capataina@users.noreply.github.com>
2024-10-15 00:07:17 +0100

    added per-tag notes

--- src/app/appComp/ToDoList.tsx
+++ src/app/appComp/ToDoList.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ToDoListColumn from './ToDoListColumn';
+import useNoteStore from "@/app/appComp/NoteStore";
 
 const ToDoList = () => {
+    const notes = useNoteStore((state) => state.notes)
+
     return (
         <div className="todo-list-container">
             <div className="todo-list">
-                <ToDoListColumn title="Planning"/>
-                <ToDoListColumn title="Working"/>
-                <ToDoListColumn title="Done"/>
+                <ToDoListColumn title="Planning" notes={notes.filter(note => note.tag === "Planning")}/>
+                <ToDoListColumn title="Working" notes={notes.filter(note => note.tag === "Working")}/>
+                <ToDoListColumn title="Done" notes={notes.filter(note => note.tag === "Done")}/>
             </div>
         </div>
     );
